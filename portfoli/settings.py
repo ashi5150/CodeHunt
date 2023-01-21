fb7e8974eab80736d21ffb671a3a6e93685d0d9a
Codehunt <aroraashima326@gmail.com>
2023-01-21 11:39:00 +0530

    intial commit

--- portfoli/settings.py
+++ portfoli/settings.py
@@ -127,10 +127,3 @@ import os
 STATICFILES_DIRS = [
      os.path.join(BASE_DIR,'static')
 ]
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER ='aroraashima326@gmail.com'
-EMAIL_HOST_PASSWORD = 'Nikash@20002001'
